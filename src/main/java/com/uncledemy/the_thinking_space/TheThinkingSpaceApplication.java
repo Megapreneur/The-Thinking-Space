89f4ffde4da61aea394e43bf53504f4d5972d006
Ademola.Megbabi <ademola.megbabi@interswitchng.com>
2025-02-02 22:20:34 +0100

    first commit

---
+++ src/main/java/com/uncledemy/the_thinking_space/TheThinkingSpaceApplication.java
@@ -0,0 +1,13 @@
+package com.uncledemy.the_thinking_space;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheThinkingSpaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheThinkingSpaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uncledemy/the_thinking_space/TheThinkingSpaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uncledemy.the_thinking_space;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheThinkingSpaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
